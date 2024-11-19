67dbaf454afea53cb0241e56901f3d31e06bccbb
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-11-19 14:58:03 +0800

    Update sketch_nov19c.ino

--- sketch_nov19c.ino
+++ sketch_nov19c.ino
@@ -1,1 +1,37 @@
 
+void setup() {
+  pinMode(14, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  tone(14, 1568, 500);
+  delay(600);
+  tone(14, 1319, 500);
+  delay(600);
+  tone(14, 1319, 500);
+  delay(600);
+  tone(14, 1397, 500);
+  delay(600);
+  tone(14, 1175, 500);
+  delay(600);
+  tone(14, 1175, 500);
+  delay(600);
+  
+  tone(14, 1047, 500);
+  delay(600);
+  tone(14, 1175, 500);
+  delay(600);
+  tone(14, 1319, 500);
+  delay(600);
+  tone(14, 1397, 500);
+  delay(600);
+  tone(14, 1568, 500);
+  delay(600);
+  tone(14, 1568, 500);
+  delay(600);
+  tone(14, 1568, 500);
+  delay(600);
+  
+  delay(2000);
+}
